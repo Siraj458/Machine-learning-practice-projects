aac20dbc1e1d8c865f1b2ef88709c79f1c1e923e
Siraj Bhattarai <47959852+Siraj458@users.noreply.github.com>
2022-04-14 22:49:22 +0545

    Add files via upload

---
+++ Jupyter notebook basics/python.py
@@ -0,0 +1,14 @@
+import math
+theNewNumber = 4
+
+def Saying():
+    print('karm ache karo, fal ki ma ki *, ghar se le lenge')
+
+    
+    
+    
+    
+    
+    
+def square_root(value):
+    return math.sqrt(value)
